710826e6b9dbc5bc7b4a132b2925a80c2dad0c72
horrible-dong <954513308@qq.com>
2023-06-15 23:57:43 +0800

    [20230615] Minor updates

--- check_for_updates.py
+++ check_for_updates.py
@@ -6,20 +6,20 @@ from termcolor import cprint
 from qtcls import __version__, __git_url__
 
 
-def check_for_updates(git_url):
+def check_for_updates(current_version: str, git_url: str):
     print(f'\nFetching the latest release version from {git_url} ...\n')
     user, repo = git_url.split('/')[-2:]
     api_url = f'https://api.github.com/repos/{user}/{repo}/releases/latest'
     response = requests.get(api_url)
     if response.status_code == 200:
         release_info = response.json()
-        __latest_version__ = release_info['tag_name']
-        cprint(f'current version: {__version__}', 'light_green', attrs=['bold'], end='  ')
-        cprint(f'latest version: {__latest_version__}', 'light_magenta', attrs=['bold'])
+        latest_version = release_info['tag_name']
+        cprint(f'current version: {current_version}', 'light_green', attrs=['bold'], end='  ')
+        cprint(f'latest version: {latest_version}', 'light_magenta', attrs=['bold'])
     else:
         print('Failed to fetch the latest release version.')
     print('\n')
 
 
 if __name__ == '__main__':
-    check_for_updates(__git_url__)
+    check_for_updates(__version__, __git_url__)
--- qtcls/utils/misc.py
+++ qtcls/utils/misc.py
@@ -484,3 +484,10 @@ def partial_class_call(cls, *partial_call_args, **partial_call_kwargs):
             return super().__call__(*self.partial_call_args, *call_args, **self.partial_call_kwargs, **call_kwargs)
 
     return PartialClassCall
+
+
+def inverse_sigmoid(x, eps=1e-3):
+    x = x.clamp(min=0, max=1)
+    x1 = x.clamp(min=eps)
+    x2 = (1 - x).clamp(min=eps)
+    return torch.log(x1 / x2)
