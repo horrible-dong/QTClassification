105cabc3607144df72be464a1f5a3b6358272d8c
horrible-dong <954513308@qq.com>
2023-08-14 18:19:11 +0800

    [20230814] Define current_time() in qtcls/utils/misc.py

--- qtcls/utils/misc.py
+++ qtcls/utils/misc.py
@@ -496,3 +496,9 @@ def inverse_sigmoid(x, eps=1e-3):
     x1 = x.clamp(min=eps)
     x2 = (1 - x).clamp(min=eps)
     return torch.log(x1 / x2)
+
+
+def current_time():
+    date_time = datetime.datetime.now()
+    return f'{date_time.year:04d}-{date_time.month:02d}-{date_time.day:02d}_' \
+           f'{date_time.hour:02d}-{date_time.minute:02d}-{date_time.second:02d}'
