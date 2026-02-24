c8c23e56bd5459db1394042b7c95a6f9730f26f7
horrible-dong <954513308@qq.com>
2026-02-24 18:28:56 +0800

    [20260224] Fix compatibility issues in log_plot.py; Optimize the code

--- qtcls/utils/log_plot.py
+++ qtcls/utils/log_plot.py
@@ -3,38 +3,41 @@
 # Copyright (c) QIU Tian. All rights reserved.
 # -------------------------------------------------------------------------
 # https://github.com/facebookresearch/detr/blob/main/util/plot_utils.py
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 # -------------------------------------------------------------------------
 
 """
 Plotting utilities to visualize training logs.
-Requires pandas<1.4.0.
 """
 
 import math
-from pathlib import Path, PurePath
+import os
 
-import numpy as np
 import pandas as pd
 import seaborn as sns
-import torch
 from matplotlib import pyplot as plt
 
+from qtcls.utils.decorators import main_process_only
 
+
+@main_process_only
 def plot_logs(logs,
               loss_fields=('loss', 'loss_ce_unscaled'),
               metric_fields=('Accuracy', 'Recall', 'Precision', 'F1-Score'),
-              ewm_col=0,
-              log_name='log.txt',
+              ewm_com=0,
               n_cols=3,
+              log_name='log.txt',
               output_file='./plot_logs.pdf'):
     """
     Function to plot specific fields from training log(s). Plots both training and test results.
 
-    :: Inputs - logs = list containing Path objects, each pointing to individual dir with a log file
-              - fields = which results to plot from each log file - plots both training and test for each field.
-              - ewm_col = optional, which column to use as the exponential weighted smoothing of the plots
-              - log_name = optional, name of log file if different than default 'log.txt'.
+    :: Inputs - logs = list of paths, each pointing to individual dir with a log file, or a single path.
+              - loss_fields = which results to plot from both training and test losses of each log file.
+              - metric_fields = which results to plot from the `test_eval` list of each log file.
+              - ewm_com = specify the degree of the exponential weighted smoothing of the plots.
+              - n_cols = number of columns in the plots.
+              - log_name = name of the log file.
+              - output_file = path of the output file.
 
     :: Outputs - matplotlib plots of results in fields, color coded for each log file.
                - solid lines are training results, dashed lines are test results.
@@ -42,32 +45,23 @@ def plot_logs(logs,
     """
     func_name = "log_plot.py::plot_logs"
 
-    # verify logs is a list of Paths (list[Paths]) or single Pathlib object Path,
-    # convert single Path to list to avoid 'not iterable' error
-
     if not isinstance(logs, list):
-        if isinstance(logs, PurePath):
+        if isinstance(logs, str):
             logs = [logs]
-            print(f"{func_name} info: logs param expects a list argument, converted to list[Path].")
         else:
-            raise ValueError(f"{func_name} - invalid argument for logs parameter.\n \
-            Expect list[Path] or single Path obj, received {type(logs)}")
+            raise TypeError(f"{func_name} - invalid argument for logs parameter.\n \
+            Expect list[str] or single str, received {type(logs)}")
 
-    # Quality checks - verify valid dir(s), that every item in list is Path object, and that log_name exists in each dir
-    for i, dir in enumerate(logs):
-        if not isinstance(dir, PurePath):
-            raise ValueError(f"{func_name} - non-Path object in logs argument of {type(dir)}: \n{dir}")
-        if not dir.exists():
-            raise ValueError(f"{func_name} - invalid directory in logs argument:\n{dir}")
-        # verify log_name exists
-        fn = Path(dir / log_name)
-        if not fn.exists():
-            print(f"-> missing {log_name}.  Have you gotten to Epoch 1 in training?")
-            print(f"--> full path of missing log file: {fn}")
-            return
+    for dir in logs:
+        if not os.path.exists(dir):
+            print(f"--> missing directory: {dir}")
+            continue
+        fn = os.path.join(dir, log_name)
+        if not os.path.exists(fn):
+            print(f"--> missing log file: {fn}")
 
     # load log file(s) and plot
-    dfs = [pd.read_json(Path(p) / log_name, lines=True) for p in logs]
+    dfs = [pd.read_json(os.path.join(p, log_name), lines=True) for p in logs]
 
     fields = loss_fields + metric_fields
 
@@ -80,25 +74,25 @@ def plot_logs(logs,
         for n, field in enumerate(fields):
             i, j = n // n_cols, n % n_cols
             if field in metric_fields:
-                coco_eval = pd.DataFrame(
-                    np.stack(df.test_eval.dropna().values)[:, metrics[field]]
-                ).ewm(com=ewm_col).mean()
-                axs[i, j].plot(coco_eval, c=color, )
+                df['test_eval'].apply(pd.Series)[metrics[field]].ewm(com=ewm_com).mean().plot(
+                    ax=axs[i, j],
+                    color=color
+                )
             else:
-                df.interpolate().ewm(com=ewm_col).mean().plot(
+                df.drop('test_eval', axis=1).ewm(com=ewm_com).mean().plot(
                     y=[f'train_{field}', f'test_{field}'],
                     ax=axs[i, j],
-                    color=[color] * 2,
-                    style=['-', '--'],
+                    color=[color, color],
+                    style=['-', '--']
                 )
 
     for ax, field in zip(axs.ravel(), fields):
         if field in metric_fields:
-            ax.legend([Path(p).name for p in logs], loc=4)
+            ax.legend([os.path.basename(p) for p in logs], loc=4)
         else:
             legend = []
             for p in logs:
-                legend.extend([f'{Path(p).name}_train', f'{Path(p).name}_test'])
+                legend.extend([f'{os.path.basename(p)}_train', f'{os.path.basename(p)}_test'])
             ax.legend(legend)
         ax.set_title(field)
 
@@ -107,46 +101,12 @@ def plot_logs(logs,
     # plt.show()
 
 
-def plot_precision_recall(files, naming_scheme='iter'):
-    if naming_scheme == 'exp_id':
-        # name becomes exp_id
-        names = [f.parts[-3] for f in files]
-    elif naming_scheme == 'iter':
-        names = [f.stem for f in files]
-    else:
-        raise ValueError(f'not supported {naming_scheme}')
-    fig, axs = plt.subplots(ncols=2, figsize=(16, 5))
-    for f, color, name in zip(files, sns.color_palette("Blues", n_colors=len(files)), names):
-        data = torch.load(f)
-        # precision is n_iou, n_points, n_cat, n_area, max_det
-        precision = data['precision']
-        recall = data['params'].recThrs
-        scores = data['scores']
-        # take precision for all classes, all areas and 100 detections
-        precision = precision[0, :, :, 0, -1].mean(1)
-        scores = scores[0, :, :, 0, -1].mean(1)
-        prec = precision.mean()
-        rec = data['recall'][0, :, 0, -1].mean()
-        print(f'{naming_scheme} {name}: mAP@50={prec * 100: 05.1f}, ' +
-              f'score={scores.mean():0.3f}, ' +
-              f'f1={2 * prec * rec / (prec + rec + 1e-8):0.3f}'
-              )
-        axs[0].plot(recall, precision, c=color)
-        axs[1].plot(recall, scores, c=color)
-
-    axs[0].set_title('Precision / Recall')
-    axs[0].legend(names)
-    axs[1].set_title('Scores / Recall')
-    axs[1].legend(names)
-    return fig, axs
-
-
 if __name__ == '__main__':
-    # Each 'Path' points to individual dir with a log file
+    # Each path points to individual dir with a log file
     plot_logs([
 
-        # Path(r"/path/to/your/dir_1"),
-        # Path(r"/path/to/your/dir_2"),
-        # Path(r"/path/to/your/dir_3"),
+        # r"/path/to/your/dir_1",
+        # r"/path/to/your/dir_2",
+        # r"/path/to/your/dir_3",
 
     ], output_file='./plot_logs.pdf')
