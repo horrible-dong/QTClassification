dac449e78bd3530594237c0ebba4c7048a2fbe0c
horrible-dong <954513308@qq.com>
2026-03-07 00:41:51 +0800

    [20260307] Check inconsistent fields between logs

--- qtcls/utils/log_plot.py
+++ qtcls/utils/log_plot.py
@@ -22,7 +22,7 @@ from qtcls.utils.decorators import main_process_only
 
 
 @main_process_only
-def plot_logs(logs,
+def plot_logs(log_dirs,
               n_cols=4,
               ewm_com=0,
               drop_cols=('epoch', 'n_params'),
@@ -31,7 +31,7 @@ def plot_logs(logs,
     """
     Function to plot specific fields from training log(s). Plots both training and test results.
 
-    :: Inputs - logs = list of paths, each pointing to an individual dir with a log file, or a single path.
+    :: Inputs - log_dirs = list of paths, each pointing to an individual dir with a log file, or a single path.
               - n_cols = number of columns in the plots.
               - ewm_com = specify the degree of the exponential weighted smoothing of the plots.
               - drop_cols = columns to drop from the plots.
@@ -44,37 +44,45 @@ def plot_logs(logs,
     """
     func_name = 'log_plot.py::plot_logs'
 
-    if not isinstance(logs, list):
-        if isinstance(logs, str):
-            logs = [logs]
+    if not isinstance(log_dirs, list):
+        if isinstance(log_dirs, str):
+            log_dirs = [log_dirs]
         else:
-            raise TypeError(f'{func_name} - invalid argument for logs parameter.\n \
-            Expect list[str] or single str, received {type(logs)}')
+            raise TypeError(f"{func_name} - expected 'log_dirs' to be list[str] or str, "
+                            f"but got {type(log_dirs).__name__}")
 
-    _logs = []
-    for dir in logs:
-        file = os.path.join(dir, log_name)
-        if not os.path.exists(dir):
-            cprint(f'{func_name} - missing log dir: {dir}', 'light_yellow')
-        elif not os.path.exists(file):
-            cprint(f'{func_name} - missing log file: {file}', 'light_yellow')
+    _log_dirs = []
+    for log_dir in log_dirs:
+        log_file = os.path.join(log_dir, log_name)
+        if not os.path.exists(log_dir):
+            cprint(f'{func_name} - missing log dir: {log_dir}', 'light_yellow')
+        elif not os.path.exists(log_file):
+            cprint(f'{func_name} - missing log file: {log_file}', 'light_yellow')
         else:
-            _logs.append(dir)
-    logs = _logs
+            _log_dirs.append(log_dir)
+    log_dirs = _log_dirs
 
-    if len(logs) == 0:
+    if len(log_dirs) == 0:
         cprint(f'{func_name} - no log files found', 'light_yellow')
         return
 
-    dfs = [pd.read_json(os.path.join(p, log_name), lines=True).drop(list(drop_cols), axis=1, errors='ignore')
-           for p in logs]
+    dfs = [pd.read_json(os.path.join(dir, log_name), lines=True).drop(list(drop_cols), axis=1, errors='ignore')
+           for dir in log_dirs]
 
-    fields = _merge_train_test_fields(dfs[0].columns)
+    _dfs = []
+    for df, log_dir in zip(dfs, log_dirs):
+        if df.columns.tolist() != dfs[-1].columns.tolist():
+            cprint(f'{func_name} - inconsistent fields between {log_dir} (ignored) and {log_dirs[-1]}', 'light_yellow')
+        else:
+            _dfs.append(df)
+    dfs = _dfs
+
+    fields = _merge_train_test_fields(dfs[-1].columns)
 
     n_rows = math.ceil(len(fields) / n_cols)
     fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(6 * n_cols, 5 * n_rows))
 
-    for df, color in zip(dfs, sns.color_palette(n_colors=len(logs))):
+    for df, color in zip(dfs, sns.color_palette(n_colors=len(log_dirs))):
         df = df.ewm(com=ewm_com).mean()
         for n, field in enumerate(fields):
             i, j = n // n_cols, n % n_cols
@@ -85,9 +93,9 @@ def plot_logs(logs,
 
     for ax, field in zip(axs.ravel(), fields):
         if len(field) == 1:
-            ax.legend([os.path.basename(p) for p in logs], loc=0)
+            ax.legend([os.path.basename(log_dir) for log_dir in log_dirs], loc=0)
         elif len(field) == 2:
-            ax.legend([f'{os.path.basename(p)}_{suffix}' for p in logs for suffix in ['train', 'test']], loc=0)
+            ax.legend([f'{os.path.basename(log_dir)}_{t}' for log_dir in log_dirs for t in ['train', 'test']], loc=0)
         else:
             ...
         ax.set_title(field[0].replace('train_', '').replace('test_', ''))
