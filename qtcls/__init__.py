be345974afc6fbf011099b582829b7666d6ca540
horrible-dong <954513308@qq.com>
2026-03-09 00:25:53 +0800

    [20260309] Version upgrade -> v0.13.1

--- qtcls/__init__.py
+++ qtcls/__init__.py
@@ -3,7 +3,7 @@
 # ********************************************
 # Copyright (c) QIU Tian. All rights reserved.
 
-__version__ = '0.13.0'
+__version__ = '0.13.1'
 __git_url__ = 'https://github.com/horrible-dong/QTClassification'
 
 from .criterions import build_criterion
